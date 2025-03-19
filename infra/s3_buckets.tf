902bdd37518e5c2c789d3e71661f38c77f97feea
David Armendariz <david.adrian.a.p@gmail.com>
2025-03-19 17:56:27 -0500

    Adds another s3 bucket for multimodal outputs

--- infra/s3_buckets.tf
+++ infra/s3_buckets.tf
@@ -15,3 +15,22 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "knowledgebase_buc
     }
   }
 }
+
+resource "aws_s3_bucket" "knowledgebase_bucket_outputs" {
+  bucket        = "${var.bucket_name}-outputs-${local.account_id}"
+  force_destroy = true
+
+  tags = {
+    Name = var.bucket_name
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "knowledgebase_bucket_output" {
+  bucket = aws_s3_bucket.knowledgebase_bucket_outputs.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
